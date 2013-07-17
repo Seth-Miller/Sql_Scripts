67417ecd043a7a08f279299c9640f063842c0d31
Seth Miller <sethmiller.sm@gmail.com>
2013-07-17 16:20:00 -0500

    Significant changes, fixed autoextend

--- tbsize.sql
+++ tbsize.sql
@@ -1,60 +1,150 @@
-column USED_MB format 9999999.9
-column FREE_MB format 9999999.9
-column TOTAL_MB format 9999999
-column PCT_USED format 999.9
-column PCT_FREE format 999.9
-column PCT_MX_FREE format 999.9
-COLUMN TBLSPC_NAME FORMAT A15
-break on report
-compute sum label TOTAL of total_mb used_mb free_mb mx_free_mb on report
+/*
 
+Tablespace Size Summary
+Created by Seth Miller 2013/07
+Version 1.0
+
+
+This script is used to report the total tablespace size in an Oracle database.
+It includes the autoextend space as an aggregate of currently used space and
+autoextend space available.
+
+The output denomination of MB, GB or TB can be changed by adding it as a
+parameter to the script (i.e. @tbsize.sql GB). MB is the default.
+
+
+=========  Summary of output  =========  
+
+TBLSPC_NAME =		Tablespace Name
+TOTAL_(MB|GB|TB) =	Total currently allocated space
+USED_(MB|GB|TB) =	Total currently used space
+FREE_(MB|GB|TB) =	Total currently free space
+PCT_FREE =		Total currently free percent
+MX_FREE_(MB|GB|TB) =	Total autoextensible free space (including currently allocated)
+PC_MX_FREE =		Total autoextensible free percent (including currently allocated)
+
+TBLSPC_NAME               TOTAL_MB  USED_MB  FREE_MB PCT_FREE MX_FREE_MB PCT_MX_FREE
+------------------------- -------- -------- -------- -------- ---------- -----------
+SYSAUX                         690      628       62      9.0      32140        98.0
+USERS                          689      655       34      5.0      32113        98.0
+SYSTEM                         800      796        4      1.0      31972        98.0
+EXAMPLE                        110        2      108     98.0      32866       100.0
+UNDOTBS1                       235       13      222     95.0      32755       100.0
+TEMP                            93
+                          -------- -------- --------          ----------
+TOTAL                         2617     2094      430              161846
+
+*/
+
+
+
+
+SET FEEDBACK OFF
+SET VERIFY OFF
+SET SERVEROUTPUT ON
+SET TERMOUT OFF
 SET PAGESIZE 100
 SET LINESIZE 150
 
-select
-  df.tablespace_name                          "TBLSPC_NAME",
-  round (df.totalspace/1024/1024)                                "TOTAL_MB",
-  round ((df.totalspace - fs.freespace)/1024/1024,1)               "USED_MB",
-  round (fs.freespace/1024/1024,1)                                 "FREE_MB",
---  round (100 * ((df.totalspace - fs.freespace) / df.totalspace),1) "PCT_USED",
-  round (100 * (fs.freespace / df.totalspace),1) "PCT_FREE",
-  case when ( df.totalmaxspace = 0 OR df.tablespace_name like 'UNDO%' )
-    then 0
-    else round ((df.totalmaxspace - (df.totalspace - fs.freespace))/1024/1024,1)
-    end "MX_FREE_MB",
-  case when ( df.totalmaxspace = 0 OR df.tablespace_name like 'UNDO%' )
-    then 0
-    else round (100 * ((df.totalmaxspace - (df.totalspace - fs.freespace))/df.totalmaxspace),1)
-    end "PCT_MX_FREE"
-from
-   (select
+-- Prevent prompting for parameter values
+COLUMN p1 new_value 1
+COLUMN p2 new_value 2
+COLUMN p3 new_value vardenom
+SELECT NULL p1, NULL p2, NULL p3 FROM DUAL WHERE  1=2;
+
+-- Format the parameter for consistency
+SELECT CASE 
+WHEN UPPER('&1') IN ('G','GB') THEN 'GB'
+WHEN UPPER('&1') IN ('T','TB') THEN 'TB'
+ELSE 'MB'
+END p3
+FROM DUAL;
+
+SET TERMOUT ON
+
+COLUMN USED_&vardenom FORMAT 9999999
+COLUMN FREE_&vardenom FORMAT 9999999
+COLUMN TOTAL_&vardenom FORMAT 9999999
+COLUMN PCT_USED FORMAT 999.9
+COLUMN PCT_FREE FORMAT 999.9
+COLUMN PCT_MX_FREE FORMAT 999.9
+COLUMN TBLSPC_NAME FORMAT A25
+BREAK ON REPORT
+COMPUTE SUM LABEL TOTAL OF total_&vardenom used_&vardenom free_&vardenom mx_free_&vardenom ON REPORT
+
+SELECT
+  df.tablespace_name										"TBLSPC_NAME",
+  CASE '&vardenom'
+  WHEN 'GB'
+    THEN ROUND (df.totalspace / 1024 / 1024 / 1024)
+  WHEN 'TB'
+    THEN ROUND (df.totalspace / 1024 / 1024 / 1024 / 1024)
+  ELSE
+    ROUND (df.totalspace / 1024 / 1024)
+  END												"TOTAL_&vardenom",
+  CASE '&vardenom'
+  WHEN 'GB'
+    THEN ROUND ((df.totalspace - fs.freespace) / 1024 / 1024 / 1024)
+  WHEN 'TB'
+    THEN ROUND ((df.totalspace - fs.freespace) / 1024 / 1024 / 1024 / 1024)
+  ELSE
+    ROUND ((df.totalspace - fs.freespace) / 1024 / 1024)
+  END												"USED_&vardenom",
+  CASE '&vardenom'
+  WHEN 'GB'
+    THEN ROUND (fs.freespace / 1024 / 1024 / 1024)
+  WHEN 'TB'
+    THEN ROUND (fs.freespace / 1024 / 1024 / 1024 / 1024)
+  ELSE
+    ROUND (fs.freespace / 1024 / 1024)
+  END												"FREE_&vardenom",
+  ROUND (100 * (fs.freespace / df.totalspace))							"PCT_FREE",
+  CASE '&vardenom'
+  WHEN 'GB'
+    THEN ROUND ((df.totalmaxspace - (df.totalspace - fs.freespace)) / 1024 / 1024 / 1024)
+  WHEN 'TB'
+    THEN ROUND ((df.totalmaxspace - (df.totalspace - fs.freespace)) / 1024 / 1024 / 1024 / 1024)
+  ELSE
+    ROUND ((df.totalmaxspace - (df.totalspace - fs.freespace)) / 1024 / 1024)
+  END												"MX_FREE_&vardenom",
+  ROUND (100 * ((df.totalmaxspace - (df.totalspace - fs.freespace)) / df.totalmaxspace))	"PCT_MX_FREE"
+FROM
+   (SELECT
       tablespace_name,
-      sum(bytes) TotalSpace,
-      sum(maxbytes) TotalMaxSpace
-   from
+      SUM(bytes) TotalSpace,
+      SUM(CASE WHEN maxbytes != 0 THEN maxbytes ELSE bytes END) TotalMaxSpace
+   FROM
       dba_data_files
-   group by
+   GROUP BY
       tablespace_name
-   union
-   select
+   UNION
+   SELECT
       tablespace_name,
-      sum(bytes) TotalSpace,
-      sum(maxbytes) TotalMaxSpace
-   from
+      SUM(bytes) TotalSpace,
+      CASE WHEN SUM(maxbytes) = 0 THEN SUM(bytes) END TotalMaxSpace
+   FROM
       dba_temp_files
-   group by
+   GROUP BY
       tablespace_name
    ) df,
-   (select
+   (SELECT
       tablespace_name,
-      sum(bytes) FreeSpace,
+      SUM(bytes) FreeSpace,
       0
-   from
+   FROM
       dba_free_space
-   group by
+   GROUP BY
       tablespace_name
    ) fs
-where
+WHERE
    df.tablespace_name = fs.tablespace_name(+)
-order by ((df.TotalSpace-fs.FreeSpace)/df.TotalSpace) desc
+order by pct_mx_free
 /
+
+-- Reset a few sqlplus parameters
+SET FEEDBACK ON
+SET VERIFY ON
+UNDEFINE 1
+UNDEFINE 2
+UNDEFINE 3
+UNDEFINE vardenom
